6795733f3d843e1acf56ece0b39809825a8e0f11
itsabhi018 <abhishekkukreja2003@gmail.com>
2026-02-11 11:52:36 +0530

    Initial commit

---
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -0,0 +1,32 @@
+package com.ecommerce.project.model;
+
+public class Category {
+    private Long categoryId;
+    private String categoryName;
+
+    // default constructor is needed by JACKSON for creating objects and later setting their values using setters
+    public Category(){
+
+    }
+
+    public Category(Long categoryId, String categoryName) {
+        this.categoryId = categoryId;
+        this.categoryName = categoryName;
+    }
+
+    public Long getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(Long categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
