39013d3f818e0f16439404b029a1d99d312181d1
itsabhi018 <abhishekkukreja2003@gmail.com>
2026-03-06 11:56:33 +0530

    Implemented user and address relationship

--- src/main/java/com/ecommerce/project/model/Address.java
+++ src/main/java/com/ecommerce/project/model/Address.java
@@ -1,15 +1,60 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.apache.catalina.users.GenericRole;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
+@Table(name="addresses")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Address {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long addressId;
+
+    @NotBlank
+    @Size(min = 5, message = "Street name must have at least 5 characters")
+    private String street;
+
+    @NotBlank
+    @Size(min = 5, message = "Building name must have at least 5 characters")
+    private String buildingName;
+
+    @NotBlank
+    @Size(min = 4, message = "City name must have at least 4 characters")
+    private String city;
+
+    @NotBlank
+    @Size(min = 3, message = "State name must have at least 3 characters")
+    private String state;
+
+    @NotBlank
+    @Size(min = 3, message = "Country name must have at least 3 characters")
+    private String country;
+
+    @NotBlank
+    @Size(min = 6, message = "PinCode must have at least 5 characters")
+    private String pinCode;
+
+    @ManyToMany(mappedBy = "addresses")
+    private List<User> users = new ArrayList<>();
+
+    public Address(String street, String buildingName, String city, String state, String country, String pinCode) {
+        this.street = street;
+        this.buildingName = buildingName;
+        this.city = city;
+        this.state = state;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
 }
