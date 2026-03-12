cbbcc899565752c4b1e0bfcdcafadcb1fb329bc9
itsabhi018 <abhishekkukreja2003@gmail.com>
2026-03-12 23:51:02 +0530

    Fixing Loose ends

--- src/main/java/com/ecommerce/project/controller/AuthController.java
+++ src/main/java/com/ecommerce/project/controller/AuthController.java
@@ -24,11 +24,15 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
 import java.util.stream.Collectors;
 
-@Slf4j
+//@Slf4j
+@RestController
+@RequestMapping("/api/auth")
 public class AuthController {
 
     @Autowired
